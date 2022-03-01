9e3d3a30a6cfb9adcdb54a7d2ce56265df19b969
nkilm <nikhilmohitelhs@gmail.com>
2022-03-01 15:42:45 +0530

    send booking info to server

--- client.py
+++ client.py
@@ -10,6 +10,14 @@ server_res = c_socket.recv(1024).decode()
 
 print(f"SERVER: {server_res}")
 
+src = input("Enter Source:")
+dest = input("Enter destination: ")
+date = input("Enter date of journey: ")
+
+c_socket.send(bytes(src,'utf-8'))
+c_socket.send(bytes(dest,'utf-8'))
+c_socket.send(bytes(date,'utf-8'))
+
 
 
 
--- server.py
+++ server.py
@@ -17,4 +17,15 @@ while True:
     # sending info to client
     c_socket.send(bytes("STATUS 200 OK","utf-8"))
 
+    print("##############################################################")
+    print("----------------------Flight Booking--------------------------")
+    print("##############################################################")
+    src = c_socket.recv(1024).decode()
+    dest = c_socket.recv(1024).decode()
+    date = c_socket.recv(1024).decode()
+    print(f"Source: {src}\nDestination: {dest}\nDate Of Journey: {date}")
+
+    print("\nAvailable Flights:\n")
+    print("foobarbaz")
+
     c_socket.close()
