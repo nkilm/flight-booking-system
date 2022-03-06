dbe33202bdaff3578242a0a758c16b2b5da1ae65
nkilm <nikhilmohitelhs@gmail.com>
2022-03-06 12:28:20 +0530

    adding code for airline api

---
+++ airline_api.py
@@ -0,0 +1,28 @@
+"""
+Checkout Airport Codes at
+https://www.nationsonline.org/oneworld/IATA_Codes/airport_code_list.htm
+
+NOTE: 
+GET NEW ACCESS TOKEN IF EXPIRED
+
+"""
+
+from requests import get
+from dotenv import load_dotenv
+from os import environ
+
+load_dotenv()
+
+src = input("Enter source:").replace(" ","")
+dest = input("Enter destination:").replace(" ","")
+
+URL = f"https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode={src}&destinationLocationCode={dest}&departureDate=2022-03-21&adults=1&max=2"
+
+headers = {}
+headers["Accept"] = "application/json"
+headers["Authorization"] = "Bearer " + environ.get("AUTH")
+
+response = get(URL,headers = headers)
+
+print(response.json())
+
