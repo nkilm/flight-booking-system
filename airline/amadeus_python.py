77f25853c48c800cf8ab92e667477295af60127d
nkilm <nikhilmohitelhs@gmail.com>
2022-03-13 16:26:42 +0530

    using simple_chalk

--- airline/amadeus_python.py
+++ airline/amadeus_python.py
@@ -2,6 +2,8 @@ from amadeus import Client, ResponseError
 from dotenv import load_dotenv
 from os import environ
 from texttable import Texttable
+from currency_converter import CurrencyConverter
+from simple_chalk import chalk
 
 load_dotenv()
 
@@ -11,21 +13,53 @@ amadeus = Client(
 )
 
 def display(res):
-  t = Texttable(max_width=0)
-  t.add_row(['ID', 'Seats','Duration','OneWay?','Departure','Arrival','Price(inc. of Taxes)'])
+  table = Texttable(max_width=0)
+  flight_id = chalk.green('ID')
+  seats = chalk.green('Seats')
+  duration = chalk.green('Duration')
+  oneway = chalk.green('OneWay?')
+  dep = chalk.green('Departure')
+  arr = chalk.green('Arrival')
+  price = chalk.green('Price(inc. of Taxes)')
+  # table.add_row([
+  #   flight_id,
+  #   seats,
+  #   duration,
+  #   oneway,
+  #   dep,
+  #   arr,
+  #   price
+  #   ])
+  table.add_row([
+    'ID',
+    'Seats',
+    'Duration',
+    'Oneway?',
+    'Departure',
+    'Arrival',
+    'Price(inc. of Taxes)'
+    ])
+  curr = CurrencyConverter()
+  
   for i in res: 
     id = i.get("id")
     seats = i.get("numberOfBookableSeats")
     duration = i.get("itineraries")[0]["duration"]
+
     price = i.get("price").get("grandTotal")
-    oneway = i.get("oneWay")
+    price_inr = chalk.green(f"₹{round(curr.convert(price, 'EUR', 'INR'),2)}")
+
+    oneway = "No" if i.get("oneWay")==0 else "Yes"
+
     departure = ""
     arrival = ""
+    
     for j in i.get("itineraries")[0]["segments"]:
-      departure += j["departure"]["iataCode"] + " " + j["departure"]["at"] + "\n"
-      arrival += j["arrival"]["iataCode"] + " " + j["arrival"]["at"] + "\n"
-    t.add_row([id,seats,duration,oneway,departure,arrival,price])
-  print(t.draw())
+      departure += j["departure"]["iataCode"] + " " + " ".join(j["departure"]["at"].split("T")) + "\n"
+      arrival += j["arrival"]["iataCode"] + " " + " ".join(j["arrival"]["at"].split("T")) + "\n"
+    table.add_row([id,seats,duration,oneway,departure,arrival,price_inr])
+
+  print(table.draw())
 
 def check_flights(src,dest,date,adults):
     try:
@@ -41,10 +75,6 @@ def check_flights(src,dest,date,adults):
 
 def confirm_price(flight_obj):
     return amadeus.shopping.flight_offers.pricing.post(flight_obj,include='credit-card-fees,other-services').data
-
-def flight_booking(flight_obj):
-    return amadeus.booking.flight_orders.post(flight_obj).data
-
 
 def book_flight(flight_obj):
     # The flight ID comes from the Flight Create Orders (in test environment it's temporary)
